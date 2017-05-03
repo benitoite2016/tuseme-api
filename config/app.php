0ee3d126dc2c6495c875709ef36af50729f89922
dontito <francistito07@gmail.com>
2017-05-03 12:42:45 +0300

    fractal conf

--- config/app.php
+++ config/app.php
@@ -162,7 +162,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-
+       Spatie\Fractal\FractalServiceProvider::class,
         /*
          * Package Service Providers...
          */
@@ -229,6 +229,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+            'Fractal' => Spatie\Fractal\FractalFacade::class,
 
     ],
 
